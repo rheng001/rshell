ea08d1bc6a643b8cf18e400ff0f19cf6e536b287
rheng001 <rheng001@gmail.com>
2015-10-20 20:32:50 -0700

    Updated

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
+#include <stdio.h> 
+#include <string>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <pwd.h>
 
 using namespace std;
 
+void userPrompt()
+{
+    
+
+}
+
+
 int main()
 {
+
     return 0;
 }
